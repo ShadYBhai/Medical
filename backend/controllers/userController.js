3c5226798bdddc5398932236a8f92aff022450d0
Ashish <ashishnick.private@gmail.com>
2022-10-30 14:36:47 +0530

    gitIgnore update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,6 +18,34 @@ const authUser = asyncHandler(async (req, res) => {
     res.status(401).json("invalid email or password");
   }
 });
+//REGISTER A NEW USER
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Alreadt Exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("INVALID USER DATA");
+  }
+});
 
 //GET user profile
 
@@ -37,4 +65,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
